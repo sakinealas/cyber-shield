5a4d46153faeff3ebc086af4380ea1e7cf6f1ca9
sakinealas <sakinealas7@gmail.com>
2025-12-22 00:26:18 +0300

    Update blacklist_manager.sh

--- scripts/blacklist_manager.sh
+++ scripts/blacklist_manager.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+set -e
+
+LOG_FILE="../auth.log"
+OUTPUT_DIR="../evidence"
+BLACKLIST_FILE="$OUTPUT_DIR/blacklisted_ips.txt"
+
+mkdir -p "$OUTPUT_DIR"
+
+echo "=== SSH FAILED LOGIN ANALYSIS ===" > "$BLACKLIST_FILE"
+
+grep "Failed password" "$LOG_FILE" \
+| awk '{print $(NF-3)}' \
+| sort \
+| uniq -c \
+| sort -nr >> "$BLACKLIST_FILE"
+
+echo "[+] Blacklist analysis completed."
 
